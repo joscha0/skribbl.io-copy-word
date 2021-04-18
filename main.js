b722733ad0425783c7b8f842ecc29c0a2ca5d3df
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-04-18 17:57:30 +0200

    add image

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 var currentWord = document.getElementById("currentWord");
 currentWord.style.userSelect = "auto";
 currentWord.style.pointerEvents = "auto";
+var image = document.createElement("img");
+image.src="https://i.postimg.cc/PrthC3LD/icon.png";
+currentWord.after(image);
